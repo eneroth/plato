1de5a80b78b5946afc177bf30ffac8cbac62df3e
Henrik Eneroth <henrik@eneroth.com>
2014-05-20 19:33:44 +0200

    Bumped version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject plato "0.1.5"
+(defproject plato "0.1.6"
   :description "Persists state to local storage."
   :url "https://github.com/eneroth/plato"
   :license {:name "Eclipse Public License"
